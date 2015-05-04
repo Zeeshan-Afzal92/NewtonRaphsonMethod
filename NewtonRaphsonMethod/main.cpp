ae8ded1a17b2641fd9fe273d9ddfea768ce68b13
Zeeshan Afzal <zeeshanafzal@184.9.168.191.isp.timbrasil.com.br>
2015-05-04 11:09:10 +0500

    Initial Commit

---
+++ NewtonRaphsonMethod/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NewtonRaphsonMethod
+//
+//  Created by Zeeshan Afzal on 04/05/2015.
+//  Copyright (c) 2015 EagleSoft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
